7b760088ee0befb1b8f317f5018ef0cd98feeecd
Gaetan GD <gaetangd@hotmail.com>
2022-03-14 17:28:59 +0100

    Starting To Fill Controllers

---
+++ src/Controller/RestaurantController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Restaurant;
+use Symfony\Component\HttpFoundation\Request;
+
+// class RestaurantController
+// {
+//     private $doctrine;
+
+//     public function __construct(ManagerRegistry $doctrine)
+//     {
+//         $this->doctrine = $doctrine;
+//     }
+// }
