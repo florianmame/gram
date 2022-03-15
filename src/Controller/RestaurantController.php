e8bd8f19085839db52cb7d35fa3ea313f60dac5e
Gaetan GD <gaetangd@hotmail.com>
2022-03-15 17:01:53 +0100

    Filled RestaurantController and its Associated Twig

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\Restaurant;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\EntityRepository;
@@ -17,25 +18,17 @@ class RestaurantController extends AbstractController {
     }
 
     public function showAll() {
-        $restaurantRepo = $this->doctrine->getRepository(Restaurant::class);
-
-        //Faire le findBy par catégorie et chosir un ordre d'affichage
-        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Africain'));
-        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Américain'));
-        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Asiatique'));
-        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Français'));
-        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Indien'));
-        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Italien'));
-        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Libanais'));
-        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Polonais'));
-        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Végétarien'));
+        $categoryRepo = $this->doctrine->getRepository(Category::class);
+        $categories = $categoryRepo->findAll();
+        
+    
 
         return $this->render(
             'restaurant/showAll.html.twig',
             [
-                "allRestaurants" => $allRestaurants,
+                "categories" => $categories,
             ]
-            );
+        );
     }
 
     public function show($idRestaurant) {
