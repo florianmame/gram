6cc5740444d648c980715b1efd07cd435c87cb27
Gaetan GD <gaetangd@hotmail.com>
2022-03-15 12:04:16 +0100

    Adding Categories in RestaurantController

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -4,9 +4,10 @@ namespace App\Controller;
 use App\Entity\Restaurant;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
-class RestaurantController {
+class RestaurantController extends AbstractController {
 
     private $doctrine;
 
@@ -19,7 +20,15 @@ class RestaurantController {
         $restaurantRepo = $this->doctrine->getRepository(Restaurant::class);
 
         //Faire le findBy par catégorie et chosir un ordre d'affichage
-        $allRestaurants = $restaurantRepo->findBy();
+        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Africain'));
+        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Américain'));
+        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Asiatique'));
+        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Français'));
+        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Indien'));
+        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Italien'));
+        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Libanais'));
+        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Polonais'));
+        $allRestaurants = $restaurantRepo->findBy(array('category' => 'Végétarien'));
 
         return $this->render(
             'restaurant/showAll.html.twig',
