0d1778d673fdfc1293e5ee9cff05fa52f97ca9f3
Gaetan GD <gaetangd@hotmail.com>
2022-03-15 10:42:15 +0100

    Deleting Layout

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Entity\Restaurant;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\HttpFoundation\Request;
 
 class RestaurantController {
@@ -18,14 +19,27 @@ class RestaurantController {
         $restaurantRepo = $this->doctrine->getRepository(Restaurant::class);
 
         //Faire le findBy par catégorie et chosir un ordre d'affichage
+        $allRestaurants = $restaurantRepo->findBy();
 
         return $this->render(
-            
-        )
+            'restaurant/showAll.html.twig',
+            [
+                "allRestaurants" => $allRestaurants,
+            ]
+            );
     }
 
     public function show($idRestaurant) {
+        $restaurantRepo = $this->doctrine->getRepository(Restaurant::class);
 
+        $restaurant = $restaurantRepo->find($idRestaurant);
+
+        return $this->render(
+            'restaurant/show.html.twig',
+            [
+                "restaurant" => $restaurant,
+            ]
+        );
     }
 
 }
