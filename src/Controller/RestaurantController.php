4b96bdef58c643e7dab66926c990f69f111e90d3
Gaetan GD <gaetangd@hotmail.com>
2022-03-15 09:47:26 +0100

    Adding Twig Files

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -2,14 +2,30 @@
 namespace App\Controller;
 
 use App\Entity\Restaurant;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 
-// class RestaurantController
-// {
-//     private $doctrine;
+class RestaurantController {
 
-//     public function __construct(ManagerRegistry $doctrine)
-//     {
-//         $this->doctrine = $doctrine;
-//     }
-// }
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    public function showAll() {
+        $restaurantRepo = $this->doctrine->getRepository(Restaurant::class);
+
+        //Faire le findBy par catégorie et chosir un ordre d'affichage
+
+        return $this->render(
+            
+        )
+    }
+
+    public function show($idRestaurant) {
+
+    }
+
+}
