921f4142d12db89dc0ad2ddccb38fd4c2988edf8
Nver Davtyan <nverdavtyan1994@gmail.com>
2022-03-22 10:42:35 +0100

    Implementation of the favorites in Homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Restaurant;
+use App\Entity\User;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -22,29 +23,41 @@ class HomeController extends AbstractController
 
     public function CategoryHome() /*showAll*/
     {
-        // Il faut avoir le répository d'une Classe d'entité pour pouvoir passer des requêtes en BDD portant sur
-        // cette entité
-        $categoryRepo = $this->doctrine->getRepository(Category::class) ;
-
-        // Ici, il y a une fonction que nous avons écrite dans categoryRepository, pour obtenir
-        //les catégories classés par date ascendante (par ordre alphabétique),
-        $restaurantRepo = $this->doctrine->getRepository(Restaurant::class);
-        // $restoRepo = $this->doctrine->getRepository(Restaurant::class);
-        $restaurant = $restaurantRepo->findAll();
+      
+      
+      //Répository de la Classe d'entité pour pouvoir passer des requêtes en BDD portant sur les users
+      $userRepo = $this->doctrine->getRepository(User::class);
+      
+      //Répository de la Classe d'entité pour pouvoir passer des requêtes en BDD portant sur les catégories
+      $categoryRepo = $this->doctrine->getRepository(Category::class);
+      
         
-        $allCategories = $categoryRepo->findBy(
+      //Répository de la Classe d'entité pour pouvoir passer des requêtes en BDD portant sur les restaurants
+      $restaurantRepo = $this->doctrine->getRepository(Restaurant::class);
+       
+      // Ici, il y a une fonction que nous avons écrite dans restaurantRepository, pour obtenir le findAll() et qu'on manipulera dans la variable $restaurant
+      $users = $userRepo->findAll();
+
+      // Ici, il y a une fonction que nous avons écrite dans restaurantRepository, pour obtenir le findAll() et qu'on manipulera dans la variable $restaurant
+      $restaurants = $restaurantRepo->findAll();
+        
+      // Ici, il y a une fonction que nous avons écrite dans categoryRepository, pour obtenir le findby() et qu'on manipulera dans la variable $allCategories
+      //les catégories sont classés par nom ascendant (par ordre alphabétique),
+      $allCategories = $categoryRepo->findBy(
           [
           ],
           [
               "name" => "ASC",
           ]
         )  ;
-
+            // On retourne les résultats de toutes les variables. Dans render on prépare l'affichage qui se fera dans home.html.twig
         return $this->render(
           'home/home.html.twig',
           [
-            "restaurant" => $restaurant ,  
+            "users" => $users,
+            "restaurants" => $restaurants ,  
             "allCategories" => $allCategories ,
+            
           ],
       ) ;
     }
