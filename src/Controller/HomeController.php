1ff635b5a2b071d90d8cf12b8551af3cc65d7d98
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-03-23 17:22:34 +0100

    Start Editing Home for Favorites

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -62,6 +62,51 @@ class HomeController extends AbstractController
       ) ;
     }
 
+    public function show()
+    {
+        
+        $restaurantRepo = $this->doctrine->getRepository(Restaurant::class);
+        $restaurants = $restaurantRepo->findAll();
+
+        $user = $this->getUser();
+        // Il faut avoir le répository d'une Classe d'entité pour pouvoir passer des requêtes en BDD portant sur
+        // cette entité
+        $userRepo = $this->doctrine->getRepository(User::class) ;
+
+        // Ici, il y a une fonction que nous avons écrite dans categoryRepository, pour obtenir
+        //les catégories classés par date ascendante (par ordre alphabétique),
+        $userInfo = $userRepo->find($user);
+
+        return $this->render(
+
+          'home/home.html.twig',
+          [
+              "restaurants" => $restaurants,
+              "user" => $userInfo ,
+          ],
+      ) ;
+    }
+
+    // On fait une fonction remove, pour supprimer les favoris du profil
+    // Il faut mettre l'id du restaurant en paramètre pour savoir quel restaurant l'utilisateur veut supprimer (ici, on type la variable pour éviter d'éventuelles erreurs)
+    public function remove(int $restaurant, RestaurantRepository $restaurantRepo){
+        // On utilise la méthode find car on ne doit trouver qu'un restaurant
+        $restaurant = $restaurantRepo->find($restaurant);
+        // On va chercher le repository de la classe User pour dialoguer avec la base de données via Doctrine
+        $userConnected = $this->getUser();
+
+        // On utilise la fonction removeRestaurant qui appartient à la classe User   
+        $userConnected->removeRestaurant($restaurant);
+
+        // On utilise l'EntityManager pour apporter des modifications à la base de données
+        $em=$this->doctrine->getManager();
+        // Avec flush, si il n'y a pas d'erreur, la modification s'exécute sur l'objet marqué : ça y est, le restaurant est supprimé des favoris
+        $em->flush();
+
+        // Après avoir exécuté cette fonction ayant pour paramètre l'id du restaurant présente dans l'URL, il faut une nouvelle route, qu'on indique ici
+        return $this->redirectToRoute('gram_main_home');
+    }
+
 
 
     
