7df2435fd81e10b42b3e25404b93257b786d72ed
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-03-23 14:37:22 +0100

    Progress on Remove Controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -51,14 +51,23 @@ class UserController extends AbstractController
       ) ;
     }
 
+    // On fait une fonction remove, pour supprimer les favoris du profil
+    // Il faut mettre idRestaurant en paramètre pour savoir quel restaurant l'utilisateur veut supprimer
     public function remove($idRestaurant){
+        // On va chercher le repository de la classe User pour dialoguer avec la base de données via Doctrine
         $restaurantRepo = $this->doctrine->getRepository(User::class);
+        // On utilise la méthode find car on ne doit trouver qu'un restaurant
+        // Du côté de la vue (show.html.twig), on ira récupérer l'id du restaurant dans l'url générée par le lien cliqué
         $restaurant = $restaurantRepo->find($idRestaurant);
 
+        // On utilise l'EntityManager pour apporter des modifications à la base de données
         $em=$this->doctrine->getManager();
+        // Dans le cas d'une suppression, on utilise remove, et non persist, pour marquer l'objet à supprimer
         $em->remove($restaurant);
+        // Avec flush, si il n'y a pas d'erreur, la modification s'exécute sur l'objet marqué : ça y est, le restaurant est supprimé des favoris
         $em->flush();
 
+        // Après avoir exécuté cette fonction ayant pour paramètre l'id du restaurant présente dans l'URL, il faut une nouvelle route, qu'on indique ici
         return $this->redirectToRoute('gram_profile_show');
     }
 
