565e73752e17b918760dfa71d506fc683b37625e
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-03-23 16:34:42 +0100

    Remove Favorites Successful

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\Restaurant;
+use App\Repository\RestaurantRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityRepository;
 
 
 class UserController extends AbstractController
@@ -46,20 +48,19 @@ class UserController extends AbstractController
 
     // On fait une fonction remove, pour supprimer les favoris du profil
     // Il faut mettre idRestaurant en paramètre pour savoir quel restaurant l'utilisateur veut supprimer
-    public function remove($idRestaurant){
-        
-        $userRestaurant = $this->getUser();
+    public function remove(int $restaurant, RestaurantRepository $restaurantRepo){
+        $restaurant = $restaurantRepo->find($restaurant);
         // On va chercher le repository de la classe User pour dialoguer avec la base de données via Doctrine
-        $userRepo = $this->doctrine->getRepository(User::class);
+        $userConnected = $this->getUser();
         // On utilise la méthode find car on ne doit trouver qu'un restaurant
         // Du côté de la vue (show.html.twig), on ira récupérer l'id du restaurant dans l'url générée par le lien cliqué
-        $userRestaurant = $userRepo->find($idRestaurant);
+        $userConnected->removeRestaurant($restaurant);
 
-        // On utilise l'EntityManager pour apporter des modifications à la base de données
+        // // On utilise l'EntityManager pour apporter des modifications à la base de données
         $em=$this->doctrine->getManager();
-        // Dans le cas d'une suppression, on utilise remove, et non persist, pour marquer l'objet à supprimer
-        $em->remove($userRestaurant);
-        // Avec flush, si il n'y a pas d'erreur, la modification s'exécute sur l'objet marqué : ça y est, le restaurant est supprimé des favoris
+        // // Dans le cas d'une suppression, on utilise remove, et non persist, pour marquer l'objet à supprimer
+        // $em->remove($userRestaurant);
+        // // Avec flush, si il n'y a pas d'erreur, la modification s'exécute sur l'objet marqué : ça y est, le restaurant est supprimé des favoris
         $em->flush();
 
         // Après avoir exécuté cette fonction ayant pour paramètre l'id du restaurant présente dans l'URL, il faut une nouvelle route, qu'on indique ici
