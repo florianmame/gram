352bb2f6ea9e04cea1dd9449f4a4e42ef2b5536c
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-03-23 16:46:30 +0100

    Remove Favorites With Comments

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -47,20 +47,19 @@ class UserController extends AbstractController
     }
 
     // On fait une fonction remove, pour supprimer les favoris du profil
-    // Il faut mettre idRestaurant en paramètre pour savoir quel restaurant l'utilisateur veut supprimer
+    // Il faut mettre l'id du restaurant en paramètre pour savoir quel restaurant l'utilisateur veut supprimer
     public function remove(int $restaurant, RestaurantRepository $restaurantRepo){
+        // On utilise la méthode find car on ne doit trouver qu'un restaurant
         $restaurant = $restaurantRepo->find($restaurant);
         // On va chercher le repository de la classe User pour dialoguer avec la base de données via Doctrine
         $userConnected = $this->getUser();
-        // On utilise la méthode find car on ne doit trouver qu'un restaurant
-        // Du côté de la vue (show.html.twig), on ira récupérer l'id du restaurant dans l'url générée par le lien cliqué
+
+        // On utilise la fonction removeRestaurant qui appartient à la classe User   
         $userConnected->removeRestaurant($restaurant);
 
-        // // On utilise l'EntityManager pour apporter des modifications à la base de données
+        // On utilise l'EntityManager pour apporter des modifications à la base de données
         $em=$this->doctrine->getManager();
-        // // Dans le cas d'une suppression, on utilise remove, et non persist, pour marquer l'objet à supprimer
-        // $em->remove($userRestaurant);
-        // // Avec flush, si il n'y a pas d'erreur, la modification s'exécute sur l'objet marqué : ça y est, le restaurant est supprimé des favoris
+        // Avec flush, si il n'y a pas d'erreur, la modification s'exécute sur l'objet marqué : ça y est, le restaurant est supprimé des favoris
         $em->flush();
 
         // Après avoir exécuté cette fonction ayant pour paramètre l'id du restaurant présente dans l'URL, il faut une nouvelle route, qu'on indique ici
