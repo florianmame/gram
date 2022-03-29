d717018b4e66d2ad2b84a5c3cb35535f1cbd672e
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-03-29 15:11:47 +0200

    Save React Search Bar

--- assets/components/Search.js
+++ assets/components/Search.js
@@ -4,26 +4,46 @@ import { useState, useEffect } from 'react/cjs/react.production.min';
 function Search() {
 
     const [ datas, setDatas ] = useState([]);
+    const [ searchTerm, setSearchTerm ] = useState("");
 
     useEffect(() => {
         fetch("")
             .then((response) => response.json())
             .then((json) => setDatas(json));
-    }, [])
+    }, []);
 
-    console.log(datas);
+    const handleSearchTerm = (event) => {
+        console.log(event.target.value);
+        let value = event.target.value;
+        setSearchTerm(value);
+    }
+
+    console.log(searchTerm);
 
     return (
         <>
             <div className='search'>
-                <input type="text" name="search-bar" id="search-bar" placeholder='Rechercher un restaurant'></input>
+                <input
+                    type="text"
+                    name="search-bar"
+                    id="search-bar"
+                    placeholder='Rechercher un restaurant'
+                    onChange={handleSearchTerm}
+                ></input>
             </div>
             <div className='search-results'>
-                <div className='search-result'>Résultats de recherche</div>
-                { datas.map(()) }
+                { datas.filter((val) => {
+                    return val.title.toLowerCase().includes(searchTerm.toLowerCase());
+                }).map((val) => {
+                    return (
+                        <div className='search-result' key={val.id}>
+                            {val.title}
+                        </div>
+                    );
+                })}
             </div>
         </>
-    )
+    );
 }
 
 export default Search
