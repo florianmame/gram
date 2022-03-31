e99c23b7bcbc2d329569b2631f6fea9ec33f913b
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-03-31 10:07:32 +0200

    Customized Fields

--- src/Controller/Admin/RestaurantCrudController.php
+++ src/Controller/Admin/RestaurantCrudController.php
@@ -5,7 +5,11 @@ namespace App\Controller\Admin;
 use App\Entity\Restaurant;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
@@ -25,14 +29,19 @@ class RestaurantCrudController extends AbstractCrudController
         return Restaurant::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
             TextField::new('name'),
             TextEditorField::new('description'),
+            TextField::new('address'),
+            TextField::new('zipCode'),
+            TextField::new('city'),
+            IntegerField::new('phone'),
+            TextEditorField::new('openingTimes'),
+            TextField::new('urlPhoto'),
         ];
     }
-    */
+
 }
