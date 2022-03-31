9fac5327cb3a310eaa906d38e737f8d92d485178
Anne Bouaniche <anne.bouaniche@protonmail.com>
2022-03-31 23:42:32 +0200

    Commenting Search.js

--- assets/components/Search.js
+++ assets/components/Search.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 function Search() {
-
+    
+    // useState est un hook permettant de gérer les states
+    // Avec setDatas, on peut vérifier que la donnée existe
+    // Ceci va nous permettre d'effectuer une recherche en temps réel (asynchrone)
     const [ datas, setDatas ] = useState([]);
     const [ searchTerm, setSearchTerm ] = useState("");
 
@@ -13,14 +16,11 @@ function Search() {
             .then((json) => setDatas(json));
     }, []);
 
+    // Ecouteur d'événement faisant le lien entre 
     const handleSearchTerm = (event) => {
-        console.log(event.target.value);
         let value = event.target.value;
         setSearchTerm(value);
     }
-
-    // Grâce à ce console.log, on peut voir que le DOM récupère bien la saisie de l'utilisateur
-    // console.log(searchTerm);
 
     return (
         <>
@@ -36,10 +36,15 @@ function Search() {
             <div className='search-results'>
                 { datas.filter((val) => {
                     if(searchTerm.length != 0) {
+                        // Le toLowerCase sert à forcer le minuscules dans la barre de saisie pour faciliter la recherche
+                        // startsWith est préférable à includes, car il ne gardera que les résultats commençant par notre saisie
                         return val.name.toLowerCase().startsWith(searchTerm.toLowerCase());
                     }
+                // La méthode map() crée un nouveau tableau avec les résultats de l'appel d'une fonction
                 }).map((val) => {
                     return (
+                        // Dans notre API, on va chercher l'objet correspondant à l'id concerné
+                        // Puis plus précisément la valeur correspondant à la clé nom, qui s'affichera à l'intérieur de la div du résultat
                         <div className='search-result' key={val.id}>
                             {val.name}
                         </div>
